42963234ce87dbd5894fce3bf42b9988e22bfef8
Dario Milanović <da7ryo@gmail.com>
2026-01-09 17:46:39 +0100

    Comments added to 3.1-calbacks.

--- 3.1-callbacks.js
+++ 3.1-callbacks.js
@@ -1,3 +1,5 @@
+// Function that takes a number and a callback function, then returns the result of the callback
+
 function processNumber(num, cb) {
   return cb(num);
 }
@@ -13,13 +15,19 @@ function multByTen(num) {
 console.log(processNumber(44, squareNum));
 console.log(processNumber(12, multByTen));
 
+// Function that takes a name and a greeting callback, then returns the greeting
+
 function greet(name, greeting) {
-  return greeting(name);
+  return greeting(name); // Call the greeting callback with name as argument
 }
 
+// Function that returns a greeting using "Hello"
+
 function greetWithHello(name) {
-  return `Hello, I am ${name}`;
+  return `Hello, I am ${name}`; // Return a formatted greeting string
 }
+
+// Function that returns a greeting using "Hi"
 
 function greetWithHi(name) {
   return `Hi there, my name is ${name}`;
